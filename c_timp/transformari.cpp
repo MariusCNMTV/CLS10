9422b824a47f3762bffb21df413b26c74cebfe97
Marius <marius.gabryel2017@gmail.com>
2023-10-16 07:55:12 +0300

    PDF

--- c_timp/transformari.cpp
+++ c_timp/transformari.cpp
@@ -2,86 +2,144 @@
 
 using namespace std;
 
-
-void prob11()
-{   
-    int n,s=1;
+void prob8()
+{
+    int s = 0, x, max = 0, min = 1000;
+    cin >> x;
+    s = s + x;
+    while (s < 1000)
+    {
+        if (x > max)
+        {
+            max = x;
+        }
+        if (x < min)
+        {
+            min = x;
+        }
+        cin >> x;
+        s = s + x;
+    }
+    cout << max << " " << min << endl;
+}
+void prob9()
+{
+    int n, x, y, m = 0;
     cin >> n;
-    int c1=n%10;
-    n = n/10;
-    int c2=n%10;
-    
-    if(c1==c2){s=0;}
-    else if(c1<c2){s=-1;}
+    while (n > 0 && y > 0)
+    {
+        x = n % 10;
+        y = n / 100 % 10;
+        if (y == 0)
+        {
+            y = x;
+        }
+        if (x % 2 != y % 2)
+        {
+            m = 1;
+        }
+        n = n / 10;
+    }
+    if (m == 0)
+    {
+        cout << "DA" << endl;
+    }
+    else
+        cout << "NU" << endl;
+}
+void prob11()
+{
+    int n, s = 1;
+    cin >> n;
+    int c1 = n % 10;
+    n = n / 10;
+    int c2 = n % 10;
 
-    while((c1-c2)*s>0 && n >9){c1 = n%10;n = n/10;c2 = n%10;}
-    
+    if (c1 == c2)
+    {
+        s = 0;
+    }
+    else if (c1 < c2)
+    {
+        s = -1;
+    }
+
+    while ((c1 - c2) * s > 0 && n > 9)
+    {
+        c1 = n % 10;
+        n = n / 10;
+        c2 = n % 10;
+    }
+
     cout << s << " " << n << endl;
-
 }
 void prob12()
 {
-    int n,x,m,p=1;
+    int n, x, m, p = 1;
     cin >> n;
-    while (x<10)
+    while (x < 10)
     {
-        int cn=n;
-        while (cn!=0)
+        int cn = n;
+        while (cn != 0)
         {
-            int c=cn%10;
-            cn = cn/10;
+            int c = cn % 10;
+            cn = cn / 10;
 
-            if (c==x)
+            if (c == x)
             {
-                m = c*p+m;
-                p = p*10;
+                m = c * p + m;
+                p = p * 10;
             }
-            
         }
-        x = x+2;
+        x = x + 2;
     }
-    cout<<m<<endl;
-    
+    cout << m << endl;
 }
 void prob13()
 {
-    int x,y,k,i,c,n;
+    int x, y, k, i, c, n;
     cin >> x >> y;
     i = x;
-    while (i<=y)
+    while (i <= y)
     {
-        n=i;
-        c=0;
-        while (n>0&&c==0)
+        n = i;
+        c = 0;
+        while (n > 0 && c == 0)
         {
-            if (n%2==1){c=1;}
-            n = n/10;
+            if (n % 2 == 1)
+            {
+                c = 1;
+            }
+            n = n / 10;
         }
-        k = k+c;
-        i = i+1;  
+        k = k + c;
+        i = i + 1;
     }
-    cout<<k<<endl;
+    cout << k << endl;
 }
 void prob14()
 {
-    int x,n,y,c;
-    while (x!=0)
+    int x, n, y, c;
+    while (x != 0)
     {
-        y=x;
-        c=0;
-        while (y>0)
+        y = x;
+        c = 0;
+        while (y > 0)
         {
-            if (y%10>c){c=y%10;}
-            y = y/10;
+            if (y % 10 > c)
+            {
+                c = y % 10;
+            }
+            y = y / 10;
         }
-        n=n*10+c;
-        cin>>x;
+        n = n * 10 + c;
+        cin >> x;
     }
-    cout<<n<<endl;
+    cout << n << endl;
 }
 
 int main()
 {
-    prob14();
+    prob9();
     return 0;
 }
--- readme.md
+++ readme.md
@@ -3,21 +3,21 @@
 using namespace std;
 ```
 
-##### 1. Calculati suma numrelor pare dintr-un numar.
+##### 1. Calculati suma numrelor pare dintr-un numar
 
-##### 2. Calculati produsul numrelor impare dintr-un numar.
+##### 2. Calculati produsul numrelor impare dintr-un numar
 
-##### 3. Se da un numar, calculati 'prientenul' acestuia.
+##### 3. Se da un numar, calculati 'prientenul' acestuia
 
 > prientenul = daca cifra este para, atunci aceasta se mareste cu o unitate, iar daca cifra este impara aceasta, scade cu o unitate.
 
     ex: n = 48965; prientenul = 59874
         n = 102; prientenul = 013
 
-> Două numere se numesc ***oglindite*** dacă fiecare se obține din celălalt, prin parcurgerea cifrelor acestuia de la dreapta la stânga.
+> Două numere se numesc **_oglindite_** dacă fiecare se obține din celălalt, prin parcurgerea cifrelor acestuia de la dreapta la stânga.
 
->Două numere se numesc ***impar-oglindite*** dacă numerele obținute din acestea, prin îndepărtarea tuturor cifrelor lor pare, sunt oglindite.
+> Două numere se numesc **_impar-oglindite_** dacă numerele obținute din acestea, prin îndepărtarea tuturor cifrelor lor pare, sunt oglindite.
 
-##### 4. Se citesc două numere naturale, x și y, și se cere să se scrie valoarea 1 dacă x și y sunt impar-oglindite sau valoarea 0 în caz contrar. Scrieți, în C++, programul de rezolvare a problemei enunțate.
+##### 4. Se citesc două numere naturale, x și y, și se cere să se scrie valoarea 1 dacă x și y sunt impar-oglindite sau valoarea 0 în caz contrar. Scrieți, în C++, programul de rezolvare a problemei enunțate
 
     ex: dacă x=523 și y=84356, se scrie 1, iar dacă x=523 și y=84536 sau x=523 și y=84576 sau x=40 și y=86, se scrie 0.
